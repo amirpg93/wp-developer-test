8ea94fe398888bf458d17fe1911d33c830c4682f
Kamlesh Jha - Server 001 <kamlesh.j@parangat.com>
2021-06-17 17:44:29 +0530

    custom file

---
+++ wp-content/plugins/customblock/customplugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package custom block
+ * @version 1.0.0
+ */
+/*
+Plugin Name: custom block
+Plugin URI: #
+Description: Basic proficiency test for application for junior wordpress developer position at kinesis.money. The test consist of simple tasks giving the applicant an opportunity to show their technical aptitude, familiarity with wordpress ecosystem, understanding of basic development practices, ability to solve problems and attention to detail.
+Author: Amir Khan
+Version: 1.0.0
+*/
